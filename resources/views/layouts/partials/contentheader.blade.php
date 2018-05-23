aa6424d77511c8674262ad233ac47010298e4800
Kevin Au <Kevin.developer71@gmail.com>
2018-05-23 17:02:30 -0400

    version 2

---
+++ resources/views/layouts/partials/contentheader.blade.php
@@ -0,0 +1,11 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        @yield('contentheader_title', 'Resumen Total')
+        <small>@yield('contentheader_description')</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Resumen</li>
+    </ol>
+</section>
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Template <strong>Copyright &copy; 2017 <a href="http://acacha.org">Acacha.org</a>.</strong> {{ trans('adminlte_lang::message.seecode') }} <a href="https://github.com/acacha/adminlte-laravel">Github</a>
+    </div>
+    <!-- Default to the left -->
+    <strong>Desarrollado por Kevin Au para los amigos traders.</strong>
+</footer>
